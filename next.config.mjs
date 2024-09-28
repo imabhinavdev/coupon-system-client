e3ff5e5381c934682c8773714a234de6e1b04f18
Abhinav Singh <abhinavas430@gmail.com>
2024-09-29 00:34:19 +0530

    Refactor next.config.mjs to update API proxy destination

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/v1/:path*", // Proxy to your external API
+        // destination: "http://localhost:8080/api/v1/:path*", // Proxy to your external API
+        destination: "https://couponsystemserver.vercel.app/api/v1/:path*", // Proxy to your external API
       },
     ];
   },
