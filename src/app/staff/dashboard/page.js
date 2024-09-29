bb68dadf0b06ac569dcb2530621ee42cb7e1e65f
Abhinav Singh <abhinavas430@gmail.com>
2024-09-30 00:58:27 +0530

    Refactor UI layout in StaffDashboard component

--- src/app/staff/dashboard/page.js
+++ src/app/staff/dashboard/page.js
@@ -3,8 +3,12 @@ import React from "react";
 
 const StaffDashboard = () => {
   return (
-    <div>
-      <QRScannerModal />
+    <div className="h-full">
+       <div className="flex flex-col items-center justify-center flex-grow h-full">
+        <h2 className="text-3xl font-bold mb-4">Tap the Button to Scan the Coupon</h2>
+        
+        <QRScannerModal />
+        </div>
     </div>
   );
 };
